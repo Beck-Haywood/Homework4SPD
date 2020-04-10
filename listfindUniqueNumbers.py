c93cb6ea3ada94e282c0edb8dd800a833e05d006
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-04-09 18:26:58 -0800

    fixed homework questions syntax

--- listfindUniqueNumbers.py
+++ listfindUniqueNumbers.py
@@ -1,9 +1,9 @@
 #Given a list of n numbers, determine if it contains any duplicate numbers.
-def findUnique(numbers)
+def findUnique(numbers):
     arr = []
-    for number in numbers
+    for number in numbers:
         if number not in arr:
             arr.append(number)
         else:
             return True
-       return False
+    return False
