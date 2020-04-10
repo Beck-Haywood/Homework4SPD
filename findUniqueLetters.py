cdd9ceec9d98b5f9ae74fb34da0f1cf9ef631eac
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-04-09 18:17:25 -0800

    added first interview googledoc paste

---
+++ findUniqueLetters.py
@@ -0,0 +1,11 @@
+def findSubstring(string):
+    arr = []
+    current_longest = []
+    for letter in string
+        if letter in arr
+            if len(current_longest) < len(arr
+                current_longest = arr
+            arr = []
+        arr.append(letter)
+    return current_longest
+print(findSubstring('qwerqtyuq u'))
